cdd0ffd1691a904f5635fe28994c7d6a0a1c1baa
Ahmed Ayman <ahmed.ayman26@msa.edu.eg>
2024-09-03 20:14:56 +0300

    fix contect bug

--- client/src/componants/contact.jsx
+++ client/src/componants/contact.jsx
@@ -20,7 +20,7 @@ export default function Contact() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch('https://formspree.io/f/', {
+        fetch('https://formspree.io/f/mgvwqopr', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
